75cc4f24b5faa06b7a350ae2310aae6404e46194
yehan <1513849675@qq.com>
2024-07-26 22:48:18 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="layout">
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.layout {
+
+		background: linear-gradient(to bottom, transparent, #fff 500rpx),linear-gradient(to right, #ff9a9e 0%, #fecfef 99%, #fecfef 100%);
+		min-height: 100vh;
+	}
+</style>
